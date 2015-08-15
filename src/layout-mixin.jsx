0725dff5a0ee79de5ad957dc48ded8114f9896a7
Jack Herrington <jherrington@walmartlabs.com>
2015-08-15 12:36:36 -0700

    Moving to 'breakpoints' and functions

--- src/layout-mixin.jsx
+++ src/layout-mixin.jsx
@@ -7,30 +7,37 @@ export default () => {
     layoutChildren(children, options, className) {
       const colMap = {1: 12, 2: 6, 3: 4, 4: 3, 6: 2, 12: 1};
 
-      const responsiveBuckets = this.props.buckets || [
-        {name:'x-small', prefix:'col-xs-'},
-        {name:'small', prefix:'col-sm-'},
-        {name:'medium', prefix:'col-md-'},
-        {name:'large', prefix:'col-lg-'}
+      const breakpoints = this.props.breakpoints || [
+        {name: 'x-small', format: (n,t) => `col-xs-${n}`},
+        {name: 'small', format: (n,t) => `col-sm-${n}`},
+        {name: 'medium', format: (n,t) => `col-md-${n}`},
+        {name: 'large', format: (n,t) => `col-lg-${n}`}
       ];
 
+      let breakpointMap = {};
+      for(var b in breakpoints) {
+        breakpointMap[breakpoints[b].name] = breakpoints[b].format;
+      }
+
       let cMap = {};
-      cMap[responsiveBuckets[0].prefix] = [12];
-      for(var b in responsiveBuckets) {
-        let bucket = responsiveBuckets[b];
+      cMap[breakpoints[0].name] = [12];
+      for(var b in breakpoints) {
+        let bucket = breakpoints[b];
         if (options[bucket.name] && colMap[options[bucket.name]]) {
-          cMap[bucket.prefix] = [colMap[options[bucket.name]]];
+          cMap[bucket.name] = [colMap[options[bucket.name]]];
         }
 
         if (options[bucket.name + '-sizes']) {
-          cMap[bucket.prefix] = options[bucket.name + '-sizes'];
+          cMap[bucket.name] = options[bucket.name + '-sizes'];
         }
       }
 
       let wrappedChildren = React.Children.map(children, (child, index) => {
         let classes = className ? [className] : [];
         for (let k in cMap) {
-          classes.push(k + cMap[k][index % cMap[k].length]);
+          var width = cMap[k][index % cMap[k].length];
+          var formatter = breakpointMap[k];
+          classes.push(formatter(width, 12));
         }
         return (<div className={classes.join(" ")}>{child}</div>);
       });
